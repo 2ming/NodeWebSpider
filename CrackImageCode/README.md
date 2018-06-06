ad18bf5c40e92266d4ab8e71535433a5fcb49c28
421543076@qq.com <421543076@qq.com>
2018-06-06 14:59:59 +0800

    图片验证码识别 `

--- CrackImageCode/README.md
+++ CrackImageCode/README.md
@@ -1,5 +1,25 @@
 ### 验证码识别
 
-``` base
-node index.js
+有几个问题：
+- Callback must be a function
+
+``` bash
+# 解决方法： https://github.com/desmondmorris/node-tesseract/issues/57
+TypeError [ERR_INVALID_CALLBACK]: Callback must be a function
+    at makeCallback (fs.js:132:11)
+    at Object.fs.unlink (fs.js:1002:14)
+    at /Users/ming/code/NodeWebSpider/node_modules/.0.2.7@node-tesseract/lib/tesseract.js:99:14
+    at FSReqWrap.readFileAfterClose [as oncomplete] (fs.js:408:3)
+
+Before:
+fs.unlink(files[0]);
+Now:
+fs.unlink(files[0], err => { if (err) console.log(err) });
+```
+
+- gm
+  > 由于 安装了 imageMagick， 所以如果直接用gm 会报错， 要改变一下
+
+``` javascript
+  let imageMagick = gm.subClass({imageMagick: true})
 ```
--- CrackImageCode/index.js
+++ CrackImageCode/index.js
@@ -1,14 +1,36 @@
-const Tesseract = require('tesseract.js')
-const myImage = require('path').resolve(__dirname, 'code.jpg');
+let fs = require('fs')
+let tesseract = require('node-tesseract');
+const gm = require('gm')
 
-Tesseract.recognize(myImage)
-  .then(data => {
-    console.log('then\n', data.text)
+const image = require('path').resolve(__dirname, 'code.jpg');
+let test_image = require('path').resolve(__dirname, 'code_1.jpg');
+
+processImg(image, test_image)
+  .then(text => {
+    console.log(`识别结果:${text}`);
   })
-  .catch(err => {
-    console.log('catch\n', err);
-  })
-  .finally(e => {
-    console.log('finally\n');
-    process.exit();
+  .catch((err) => {
+    console.error(`识别失败:${err}`);
   });
+
+function processImg(imgPath, newPath, thresholdVal) {
+  return new Promise((resolve, reject) => {
+    let imageMagick = gm.subClass({imageMagick: true})
+    imageMagick(imgPath)
+      .despeckle()
+      .contrast(-6)
+      .write(newPath, err => {
+        if (err) {
+          reject(err);
+        } else {
+          tesseract.process(newPath, {l: 'eng', psm: 7, binary: 'tesseract'}, (err, data) => {
+            if (err) {
+              reject(err)
+            } else {
+              resolve(data.trim())
+            }
+          });
+        }
+      });
+  });
+}
